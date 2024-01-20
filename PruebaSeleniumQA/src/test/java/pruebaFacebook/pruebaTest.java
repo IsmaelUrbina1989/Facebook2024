37877efcf790f7f49a2358715beb35a90362e199
user <user@DESKTOP-ISMAEL>
2024-01-20 15:31:25 -0600

    Rpimer Commit

--- PruebaSeleniumQA/src/test/java/pruebaFacebook/pruebaTest.java
+++ PruebaSeleniumQA/src/test/java/pruebaFacebook/pruebaTest.java
@@ -8,7 +8,7 @@ public class pruebaTest {
   @Test
   public void f() throws InterruptedException {
 	  WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com");
+		driver.get("https://www.linio.com");
 		Thread.sleep(3000);
 		driver.close();
   }
