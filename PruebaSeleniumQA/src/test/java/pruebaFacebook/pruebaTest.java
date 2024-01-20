1e04ee7899ae13499c8b64c80e1cb01ecf11c386
user <user@DESKTOP-ISMAEL>
2024-01-20 15:16:13 -0600

    Rpimer Commit

---
+++ PruebaSeleniumQA/src/test/java/pruebaFacebook/pruebaTest.java
@@ -0,0 +1,15 @@
+package pruebaFacebook;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class pruebaTest {
+  @Test
+  public void f() throws InterruptedException {
+	  WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		Thread.sleep(3000);
+		driver.close();
+  }
+}
